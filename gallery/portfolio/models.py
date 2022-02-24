e4c703f88e4699bc02201d4cfaafe4091792bc5c
Marcos8060 <marcosgav80@gmail.com>
2022-02-24 22:07:12 +0300

    created migrations for the database models

--- gallery/portfolio/models.py
+++ gallery/portfolio/models.py
@@ -11,8 +11,6 @@ class Location(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    class Meta:
-        ordering=('-name')
 
     def __str__(self):
         return self.name
