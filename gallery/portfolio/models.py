4d7859a13128041ecd81d5e80fead102e8c8dc2f
Marcos8060 <marcosgav80@gmail.com>
2022-02-24 22:00:24 +0300

    created the location,category and image models for the database tables

--- gallery/portfolio/models.py
+++ gallery/portfolio/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    class Meta:
+        ordering=('-name')
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    location = models.ForeignKey(Location,on_delete=models.CASCADE) # one image belongs to a single location
+    category = models.ForeignKey(Category,on_delete=models.CASCADE) # one image belongs to a single category
+    name= models.CharField(max_length=200)
+    description = models.TextField(max_length=300)
+    image = models.ImageField(upload_to = 'articles/',blank=True)
